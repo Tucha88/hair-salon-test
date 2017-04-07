15cf594d54310f7625392f40cf51bfa2e32bdeb1
Boris <borislav88@yandex.ru>
2017-04-08 00:01:32 +0300

    Update of a project. Changed some names, cleaned come methods, added some comments and TODOs

--- src/main/java/hairsaon/services/HairSalonController.java
+++ src/main/java/hairsaon/services/HairSalonController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.PathParam;
 import java.util.List;
 
 
@@ -46,16 +47,33 @@ public class HairSalonController {
         return new ResponseEntity<>(updatedMaster, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "masstersarray", method = RequestMethod.GET)
+    @RequestMapping(value = "masters", method = RequestMethod.GET)
     public ResponseEntity<List<Master>> getAllMasters() {
         return new ResponseEntity<>(masterRepositoryr.findAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "master/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Master> getMaster(@PathVariable String id) {
-        return new ResponseEntity<>(masterRepositoryr.findByEmail(id), HttpStatus.OK);
+    @RequestMapping(value = "masters/{id:.+}", method = RequestMethod.GET)
+    public ResponseEntity<Master> getMaster(@PathVariable("id") String id) {
+        Master master = masterRepositoryr.findByEmail(id);
+        if (master == null) {
+            return new ResponseEntity<Master>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(master, HttpStatus.OK);
     }
 
+    @RequestMapping(value = "clients", method = RequestMethod.GET)
+    public ResponseEntity<List<Client>> getAllClients() {
+        return new ResponseEntity<>(clientRepository.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "clients/{id:.+}", method = RequestMethod.GET)
+    public ResponseEntity<Client> getClient(@PathVariable("id") String id) {
+        Client client = clientRepository.findClientByClientEmail(id);
+        if (client == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(client, HttpStatus.OK);
+    }
     @PutMapping("updateclient")
     public ResponseEntity<Client> updateClietn(@RequestBody Client client) {
         Client updatedClient = clientRepository.findClientByClientEmail(client.getClientEmail());
