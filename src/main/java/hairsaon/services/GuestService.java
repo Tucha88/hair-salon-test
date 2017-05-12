6f05728b5698393f6010255a349aad3282c51a5c
Boris <borislav88@yandex.ru>
2017-05-13 00:15:59 +0300

    Фикс бага с паролем при логине3у

--- src/main/java/hairsaon/services/GuestService.java
+++ src/main/java/hairsaon/services/GuestService.java
@@ -9,6 +9,7 @@ import hairsaon.repository.MasterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 /**
  * Created by Boris on 10.05.2017.
  */
+@CrossOrigin
 @RestController
 @RequestMapping("guest")
 public class GuestService {
