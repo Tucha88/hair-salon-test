f60c5809f688cb8605f60cfc8ec04d6b1a5e9f15
limonlim <limon.lim@gmail.com>
2017-05-24 14:52:10 +0300

    Изменил названия переменых в LightClock

--- src/main/java/hairsaon/myExtends/LightClock.java
+++ src/main/java/hairsaon/myExtends/LightClock.java
@@ -9,15 +9,15 @@ import java.util.Calendar;
 public class LightClock implements Comparable, Serializable {
     private static final long serialVersionUID = 112234554L;
 
-    int hour;
-    int minute;
+    int hourLight;
+    int minuteLight;
 
     public LightClock() {
     }
 
     public LightClock(LightClock clock) {
-        this.hour = clock.getHour();
-        this.minute = clock.getMinute();
+        this.hourLight = clock.getHourLight();
+        this.minuteLight = clock.getMinuteLight();
     }
 
 //    public LightClock(MyClock clock) {
@@ -26,36 +26,36 @@ public class LightClock implements Comparable, Serializable {
 //    }
 
     public LightClock(int hour, int minute) {
-        this.hour = hour;
-        this.minute = minute;
+        this.hourLight = hour;
+        this.minuteLight = minute;
     }
 
-    public int getHour() {
-        return hour;
+    public int getHourLight() {
+        return hourLight;
     }
 
-    public void setHour(int hour) {
-        this.hour = hour;
+    public void setHourLight(int hourLight) {
+        this.hourLight = hourLight;
     }
 
-    public int getMinute() {
-        return minute;
+    public int getMinuteLight() {
+        return minuteLight;
     }
 
-    public void setMinute(int minute) {
-        this.minute = minute;
+    public void setMinuteLight(int minuteLight) {
+        this.minuteLight = minuteLight;
     }
 
     public void setTime(int hour, int minute) {
-        this.hour = hour;
-        this.minute = minute;
+        this.hourLight = hour;
+        this.minuteLight = minute;
     }
 
     @Override
     public boolean equals(Object obj) {
         LightClock tempClock = (LightClock) obj;
-        if (this.getHour() == tempClock.getHour()) {
-            if (this.getMinute() == tempClock.getMinute()) {
+        if (this.getHourLight() == tempClock.getHourLight()) {
+            if (this.getMinuteLight() == tempClock.getMinuteLight()) {
                 return true;
             }
         }
@@ -64,7 +64,7 @@ public class LightClock implements Comparable, Serializable {
 
     @Override
     public int hashCode() {
-        return this.getHour() * 81 + this.getMinute() * 17 - this.getHour() * 34;
+        return this.getHourLight() * 81 + this.getMinuteLight() * 17 - this.getHourLight() * 34;
     }
 
 
@@ -72,27 +72,27 @@ public class LightClock implements Comparable, Serializable {
     public int compareTo(Object obj) {
         LightClock tempClock = (LightClock) obj;
         int res;
-        res = this.getHour() - tempClock.getHour();
+        res = this.getHourLight() - tempClock.getHourLight();
         if (res == 0) {
-            res = this.getMinute() - tempClock.getMinute();
+            res = this.getMinuteLight() - tempClock.getMinuteLight();
         }
         return res;
     }
 
     public void addHour(int hour) {
-        MyClock myClock = new MyClock(this.getHour(), this.getMinute());
+        MyClock myClock = new MyClock(this.getHourLight(), this.getMinuteLight());
         myClock.add(Calendar.HOUR_OF_DAY, hour);
         this.setTime(myClock.getHour(), myClock.getMinute());
     }
 
     public void addMinute(int minute) {
-        MyClock myClock = new MyClock(this.getHour(), this.getMinute());
+        MyClock myClock = new MyClock(this.getHourLight(), this.getMinuteLight());
         myClock.add(Calendar.MINUTE, minute);
         this.setTime(myClock.getHour(), myClock.getMinute());
     }
 
     @Override
     public String toString() {
-        return hour + ":" + minute;
+        return hourLight + ":" + minuteLight;
     }
 }
